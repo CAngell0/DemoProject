d15c9664a87049bdd3af0187fedfa224d01dcfe8
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-09-09 08:46:39 -0600

    Added the scanner into the controller

--- src/demo/controller/Controller.java
+++ src/demo/controller/Controller.java
@@ -1,12 +1,20 @@
 package demo.controller;
 
+import java.util.Scanner;
+
 public class Controller {
 
-   public Controller(){
+   private Scanner keyboardInput;
 
+   public Controller(){
+      this.keyboardInput = new Scanner(System.in);
    }
 
    public void start(){
-      
+      System.out.println("Do I work?");
+      String input = keyboardInput.nextLine();
+      System.out.println("You said this: " + input);
+
+      keyboardInput.close();
    }
 }
