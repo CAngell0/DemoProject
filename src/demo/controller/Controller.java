47861bc995046ca1d3db9aeae21f876e2ff4d538
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-09-19 08:32:18 -0600

    Created practice methods for if statements

--- src/demo/controller/Controller.java
+++ src/demo/controller/Controller.java
@@ -12,17 +12,97 @@ public class Controller {
 
    public void start(){
       // askQuestions();
-      practiceStringMethods();
+      // pacticeSubstr();
+      practiceEquals();
       keyboardInput.close();
    }
 
-   private void practiceStringMethods(){
+   private void practiceUsingNestedIfs() {
+      System.out.println("Did you give the mouse a brownie?");
+      String response = keyboardInput.next();
+
+      if (response.equalsIgnoreCase("yes")){
+         System.err.println("Did you also give the mouse ice cream?");
+         response = keyboardInput.next();
+
+         if (response.toLowerCase().equals("yes")){
+            System.out.println("You gave him both");
+         } else {
+            System.out.println("You only gave him one");
+         }
+      }
+
+      System.out.println("Do you like programming?");
+      response = keyboardInput.next();
+
+      if (response.equalsIgnoreCase("yes")){
+         System.out.println("Do you like Java?");
+         response = keyboardInput.next();
+         if (response.equalsIgnoreCase("yes")){
+            System.out.println("That's great!");
+         } else {
+            System.out.println("That sucks!");
+         }
+      }
+   }
+
+   private void practiceCompoundLogicalOperations(){
+      String inputOne = "";
+      String inputTwo = "";
+
+      System.out.println("What is your favorite color?");
+      inputOne = keyboardInput.next();
+      System.out.println("What is the opposite of that color?");
+      inputTwo = keyboardInput.next();
+
+      if (inputOne.equals("blue") && inputTwo.equals("blue")){
+         System.out.println("Color Theory!");
+      }
+
+      System.out.println("Did you give the mouse a cookie?");
+      inputOne = keyboardInput.next();
+      System.out.println("Did you give the mouse some milk?");
+      inputTwo = keyboardInput.next();
+
+      if (inputOne.equals("yes") && inputTwo.equals("yes")){
+         System.out.println("Continuing...");
+      } else {
+         System.out.println("Finished");
+      }
+
+      System.out.println("Do you like programming?");
+      inputOne = keyboardInput.next();
+      System.out.println("Do you like Java?");
+      inputTwo = keyboardInput.next();
+      
+      if (inputOne.equals("yes") && inputTwo.equals("no")){
+         System.out.println("That's great!");
+      } else {
+         System.out.println("That sucks!");
+      }
+   }
+
+   private void pacticeSubstr(){
       String myName = "Carson Angell";
       System.out.println("Length of my name is: " + myName.length());
       
       //Got just my first name
-      String part = myName.substring(0, 6);
-      System.out.println(part);
+      String firstName = myName.substring(0, 6);
+      System.out.println(firstName);
+
+      int spaceLocation = myName.indexOf(" ");
+      String lastName = myName.substring(spaceLocation).trim();
+      System.out.println(lastName);
+   }
+
+   private void practiceEquals(){
+      String firstName = "Carson";
+      String lastName = "Casron";
+
+      boolean isTheSame = firstName.equals(lastName);
+
+      System.out.println("Is firstName the same as lastName?");
+      System.out.println("firstName: \"" + firstName + "\" lastName: \"" + lastName + "\" - " + isTheSame);
    }
 
    private void askQuestions(){
